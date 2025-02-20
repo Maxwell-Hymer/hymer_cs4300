8e6a0be36755d19ff8796fe6aa8f9764813f21c8
Maxwell Hymer <mhymer@uccs.edu>
2025-02-20 23:50:08 +0000

    Started adding Movie & Seat models in the bookings app

--- homework2/movie_theater_booking/bookings/models.py
+++ homework2/movie_theater_booking/bookings/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    release_date = models.DateField()
+    duration = models.DurationField()
+    def __str__(self):
+        return self.title
+
+class Seat(models.Model):
+    seat_number = models.CharField(max_length=10)
+    booking_status = models.BooleanField(default=False)
+    def __str__(self):
+        return self.seat_number
+
