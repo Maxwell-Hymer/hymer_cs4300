4591898f1563ca3097f295a9de50864c1c530609
Maxwell Hymer <mhymer@uccs.edu>
2025-02-21 20:25:24 +0000

    added api's and urls to bookings

---
+++ homework2/movie_theater_booking/bookings/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from views import MovieViewSet, SeatViewSet, BookingViewSet
+
+# Initialize a router and assign our viewsets with it
+router = DefaultRouter()
+router.register(r'movies', MovieViewSet)
+router.register(r'seats', SeatViewSet)
+router.register(r'bookings', BookingViewSet)
+
+# API URLs determined automatically by the router
+urlpatterns = [
+    path('api/', include(router.urls)),
+]
